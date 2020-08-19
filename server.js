6ed5283d3d0746b9b716b11db612c21ea3008048
DCRevResLabs <dc@revolutionresearch.com.au>
2020-08-19 21:42:22 +1000

    server refine

--- server.js
+++ server.js
@@ -1,26 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const routes = require("./routes/");
+const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
-app.use(express.urlencoded({ limit: "25mb" }, { extended: true }));
-app.use(express.json({ limit: "25mb" }));
-mongoose.set("useFindAndModify", false);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "./client/build")));
+  app.use(express.static("client/build"));
 }
-app.get("*", function (_, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"), function (
-    err
-  ) {
-    if (err) {
-      res.status(500).send(err);
-    }
-  });
-});
 // Add routes, both API and view
 app.use(routes);
 
